d726aef06352622415bed5b3276a421e4d0fdfc7
rennangalvao <inc.rennan@gmail.com>
2022-03-25 19:02:34 -0400

    Add event de click

--- asset/js/script.js
+++ asset/js/script.js
@@ -1,2 +1,10 @@
+const dino = document.querySelector('.dino');
 
+function handleKeyUp() {
+    if (event.keyCode === 32) {
+        console.log('Pressionou espaço!');
+    }
+}
+
+document.addEventListener('keyup', handleKeyUp);
 
